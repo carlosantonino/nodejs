09773dd71e08167609c3e5e682054e823bb04a2a
Carlos Antonino <carlosantonino.cs@gmail.com>
2022-09-15 14:30:59 -0300

    conexão criada

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express()
 
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'meubanco'
+})
+
+// parou no minuto 25:32 
+// https://www.youtube.com/watch?v=5JnPd_YckQw&list=PLmY5AEiqDWwBHJ3i_8MDSszXXRTcFdkSu&index=7
+
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/src/index.html")
 })
